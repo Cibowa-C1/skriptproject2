9b70c5a61c1eebeb9b53d7a4b838194ac923d628
Cibowa-C1 <nklinic5618rn@raf.rs>
2021-01-02 18:09:57 +0100

    migracije,model

--- project/poruke/models.py
+++ project/poruke/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.CharField(max_length=64)
+
+    def _str_(self):
+        return self.content
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.content
